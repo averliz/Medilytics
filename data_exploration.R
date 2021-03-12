01607c22cca52b5b96e3b76173a2c03e32a3a08a
Jeremy Teo <jimmy.teo878@gmail.com>
2021-03-12 16:05:03 +0800

    Data Exploration Work Split

--- data_exploration.R
+++ data_exploration.R
@@ -1,39 +1,54 @@
 library(tidyverse)
 library(data.table)
+# install.packages("skimr")
+library(skimr)
 
 setwd("C:/Users/jimmy/NTU/BC2407/Project")
+data <- fread('dataset.csv') # very taxing procedure
 
-full_data <- fread("dataset.csv")
+relevantcols <- fread('RelevantColumns.csv') # a .csv of the original file, with the redundant rows removed
+relevantVariableNames <- c(relevantcols$`SAS Variable Name`)
+as.data.frame(relevantVariableNames)
 
-# full_data
+actual_data <- data[, ..relevantVariableNames]
 
-disease_analysis_names <- c('BPHIGH4','TOLDHI2','CHOLCHK2', 'CVDINFR4', 'CVDCRHD4', 
-                     'CVDSTRK3', 'ASTHMA3', 'CHCSCNCR', 'CHCOCNCR', 'CHCCOPD2', 
+disease_analysis_names <- c('BPHIGH4','TOLDHI2','CHOLCHK2', 'CVDINFR4', 'CVDCRHD4',
+                     'CVDSTRK3', 'ASTHMA3', 'CHCSCNCR', 'CHCOCNCR', 'CHCCOPD2',
                      'CHCKDNY2', 'DIABETE4', 'HAVARTH4', 'PREDIAB1')
+# 
 
-full_data_tibble <- as_tibble(full_data)
-disease_data <- full_data_tibble[,disease_analysis]
 
-demo_analysis_names <- c('SEXVAR','GENHLTH','PHYSHLTH','POORHLTH','HLTHPLN1','PERSDOC2','MEDCOST',
-'CHECKUP1','MARITAL','EDUCA','RENTHOM1','VETERAN3','EMPLOY1','CHILDREN',
-'INCOME2','WEIGHT2','HEIGHT3','PREGNANT','DEAF','BLIND','SMOKE100',
-'SMOKDAY2','LASTSMK2','USENOW3','ALCDAY5','AVEDRNK3','DRNK3GE5','EXERANY2',
-'EXRACT11','STRENGTH','FRUIT2','FRUITJU2','FVGREEN1','FRENCHF1','POTATOE1',
-'VEGETAB2','HIVRISK5')
+# 'SMOKDAY2','LASTSMK2' <- these 2 var is reliant removed as it SMOKE100 is enough
+# 'AVEDRNK3','DRNK3GE5' <- ALCDAY5 is sufficient to det. if patient drinks 
 
-demo_data <- full_data_tibble[,demo_analysis_names]
 
-combined_names <- c('BPHIGH4','TOLDHI2','CHOLCHK2','CVDINFR4','CVDCRHD4',
-                    'CVDSTRK3','ASTHMA3','CHCSCNCR','CHCOCNCR','CHCCOPD2',
-                    'CHCKDNY2','DIABETE4','HAVARTH4','PREDIAB1','DEAF',
-                    'GENHLTH','PHYSHLTH','POORHLTH','HLTHPLN1','PERSDOC2',
-                    'MEDCOST','CHECKUP1','MARITAL','EDUCA','RENTHOM1',
-                    'VETERAN3','EMPLOY1','CHILDREN','INCOME2','WEIGHT2',
-                    'HEIGHT3','PREGNANT','DEAF','BLIND','SMOKE100','SMOKDAY2',
-                    'LASTSMK2','USENOW3','ALCDAY5','AVEDRNK3','DRNK3GE5',
-                    'EXERANY2','EXRACT11','STRENGTH','FRUIT2','FRUITJU2',
-                    'FVGREEN1','FRENCHF1','POTATOE1','VEGETAB2','HIVRISK5')
+medical <- data[,..disease_analysis_names]
+skim(medical)
 
-combined_data <- full_data_tibble[,combined_names]
+# JQ - 'BPHIGH4','TOLDHI2','CHOLCHK2', 'CVDINFR4', 'CVDCRHD4', 'CVDSTRK3', 'ASTHMA3', 'CHCSCNCR', 'CHCOCNCR', 'CHCCOPD2', 'CHCKDNY2', 'DIABETE4', 'HAVARTH4', 'PREDIAB1'
+# Jeremy - 'SEXVAR','GENHLTH','PHYSHLTH','POORHLTH','HLTHPLN1','PERSDOC2','MEDCOST','CHECKUP1'
+# Manika - 'MARITAL','EDUCA','RENTHOM1','VETERAN3','EMPLOY1','CHILDREN', 'INCOME2','WEIGHT2'
+# Shao Jing - 'HEIGHT3','PREGNANT','DEAF','BLIND','SMOKE100', 'USENOW3','ALCDAY5','EXERANY2'
+# JL - 'EXRACT11','STRENGTH','FRUIT2','FRUITJU2','FVGREEN1','FRENCHF1','POTATOE1', 'VEGETAB2','HIVRISK5'
+demo_analysis_names <- c('SEXVAR','GENHLTH','PHYSHLTH','POORHLTH',
+                         'HLTHPLN1','PERSDOC2','MEDCOST',
+                         'CHECKUP1','MARITAL','EDUCA','RENTHOM1','VETERAN3','EMPLOY1','CHILDREN',
+                         'INCOME2','WEIGHT2','HEIGHT3','PREGNANT','DEAF','BLIND','SMOKE100',
+                         'USENOW3','ALCDAY5','EXERANY2',
+                         'EXRACT11','STRENGTH','FRUIT2','FRUITJU2','FVGREEN1','FRENCHF1','POTATOE1',
+                         'VEGETAB2','HIVRISK5')
+non_medical <- data[,..demo_analysis_names]
 
-summary(combined_data["BPHIGH4"])
+skim(non_medical)
+
+skim(actual_data)
+
+cor(actual_data$BPHIGH4, actual_data$SEXVAR)
+# Look at blood pressure first 
+
+chaactual_data$HEIGHT3[1]
+
+################ 
+# let us predict BPHIGH$ using all the demo analysis 
+
+model1 <- lm(BPHIGH4 <- SEXVAR)
