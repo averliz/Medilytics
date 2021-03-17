c5b0646e181d3216417989f40341eb376e326df7
ManikaHennedige <mhennedige@gmail.com>
2021-03-17 17:29:54 +0800

    Change names of columns to remove underscore

--- cleaning_extractRelevantColumns.R
+++ cleaning_extractRelevantColumns.R
@@ -10,5 +10,15 @@ as.data.frame(relevantVariableNames)
 
 actual_data <- data[, ..relevantVariableNames]
 
+actual_data <- actual_data %>%
+  rename(
+    'RFHYPE5' = '_RFHYPE5',
+    'RFCHOL2' = '_RFCHOL2',
+    'MICHD' = '_MICHD',
+    'CASTHM1' = '_CASTHM1',
+    'DRDXAR2' = '_DRDXAR2',
+  )
+
+
 write.csv(actual_data, "CleanedData.csv")
 
